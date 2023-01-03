50d8d889233d278c4259ff2115ca6a0313eb79a6
usr1 <usr1@pop-os.localdomain>
2023-01-02 21:19:55 -0800

    init commit

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import MainPage from './components/MainPage'
+
+const root = createRoot(document.getElementById('root'))
+root.render(<MainPage />)
---
+++ src/components/MainPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const MainPage = () => (
+  <div>
+    <h1>Main Page</h1>
+    <p>Now in color!</p>
+  </div>
+)
+
+export default MainPage
